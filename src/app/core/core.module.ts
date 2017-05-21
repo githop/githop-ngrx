7feda4fbe51574e8851788afd901dcffcd2980e5
githop <tom@githop.com>
2017-05-21 14:53:55 -0600

    feat(layout-module, ngrx): stub out ResumeService

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import {FIREBASE} from '../../environments/firebase';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {ResumeService} from './services';
 
 
 
@@ -14,6 +15,7 @@ import {AngularFireDatabaseModule} from 'angularfire2/database';
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(FIREBASE)
   ],
-  declarations: []
+  declarations: [],
+  providers: [ResumeService]
 })
 export class CoreModule { }
