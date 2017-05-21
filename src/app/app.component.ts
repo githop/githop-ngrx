d1ed9e3c8e2daea1a3b95cdc2189ae49b66620e2
githop <tom@githop.com>
2017-05-21 14:20:54 -0600

    init commit - setup app scaffold MVP

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gth-root',
+  template: `
+    <h1>
+      {{title}}
+    </h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'gth works!';
+}
