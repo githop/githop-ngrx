09cfd4f1cbf31c08f17efc3c1d064ebd49a86d9f
githop <tom@githop.com>
2017-05-21 14:50:21 -0600

    feat(layout-module, ngrx): add layout module, scaffold ngrx directories

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FIREBASE} from '../../environments/firebase';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(FIREBASE)
   ],
   declarations: []
 })
