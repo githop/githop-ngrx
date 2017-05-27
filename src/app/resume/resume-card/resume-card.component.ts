cddec68896b116ce317c5dc8a5169e28d6d462a8
githop <tom@githop.com>
2017-05-27 15:47:04 -0600

    updates(resume): bring in CSS for resume

--- src/app/resume/resume-card/resume-card.component.ts
+++ src/app/resume/resume-card/resume-card.component.ts
@@ -9,18 +9,31 @@ type TitleMap = {
   selector: 'gth-resume-card',
   template: `    
     <ng-container *ngIf="resumeCard">
-      <div>
-        <h2>{{type}}</h2>
-        <div *ngFor="let card of resumeCard.contents">
-          <h4>{{card.title}}</h4>
-          <div [innerHtml]="card.description"></div>
-          <div *ngIf="card.accomplishments">
-            <ul>
+      
+      <div class="gth-card">
+        <div class="gth-card__header">
+          <h2>{{type}}</h2>
+        </div>
+        <div class="gth-card__body">
+          <div *ngFor="let card of resumeCard.contents; let last = last;">
+            <div class="body__info">
+              <h3>
+                <a *ngIf="card.link" [href]="card.link">{{card.title}}</a>
+                <span *ngIf="!card.link">{{card.title}}</span>
+              </h3>
+              <p>{{card.date}}</p>
+            </div>
+
+            <h4>{{card.position}}</h4>
+            <p [innerHtml]="card.description"></p>
+            <ul *ngIf="card.accomplishments">
               <li *ngFor="let accomplishment of card.accomplishments">{{accomplishment.text}}</li>
             </ul>
+            <hr *ngIf="!last">
           </div>
         </div>
       </div>
+      
     </ng-container>`,
   styles: []
 })
